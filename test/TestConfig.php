5218888e784a8b361fbce01ffa47dbb321db21f1
Carsten Bleek <bleek@cross-solution.de>
2016-01-08 20:25:32 +0100

    [Ci] uses phpunit4.7

--- test/TestConfig.php
+++ test/TestConfig.php
@@ -2,7 +2,7 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
-        'Core', 'Auth', 'Jobs', 'Organizations',  'Applications'
+        'Core', 'Auth', 'Jobs', 'Organizations',  'Applications', 'Cv'
     ),
 
     // These are various options for the listeners attached to the ModuleManager
